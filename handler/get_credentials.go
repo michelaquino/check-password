2c6a214ac8b41322cbb44488f4907112a696d996
Michel Aquino <michel.aquino@corp.globo.com>
2017-08-15 16:34:03 -0300

    List hash

--- handler/get_credentials.go
+++ handler/get_credentials.go
@@ -6,20 +6,45 @@ import (
 	"gitlab.globoi.com/michel.aquino/check-password/models"
 	"gitlab.globoi.com/michel.aquino/check-password/repository"
 
+	"github.com/asaskevich/govalidator"
 	"github.com/labstack/echo"
 )
 
 func GetCredentials(echoContext echo.Context) error {
-	return echoContext.Render(http.StatusOK, "getCredentials", "")
+	return echoContext.Render(http.StatusOK, "getCredentials", nil)
 }
 
 func PostCredentials(echoContext echo.Context) error {
 	credentials := new(models.Credentials)
 
 	if err := echoContext.Bind(credentials); err != nil {
-		return echoContext.Render(http.StatusInternalServerError, "getCredentials", "")
+		return echoContext.Render(http.StatusInternalServerError, "getCredentials", nil)
 	}
 
+	if credentials.Email == "" || !govalidator.IsEmail(credentials.Email) {
+		viewModel := GetCredentialViewModel{
+			HasError:     true,
+			ErrorMessage: "O e-mail é inválido",
+		}
+
+		return echoContext.Render(http.StatusInternalServerError, "getCredentials", viewModel)
+	}
+
+	if len(credentials.Password) < 8 {
+		viewModel := GetCredentialViewModel{
+			HasError:     true,
+			ErrorMessage: "A senha deve ter mais que 8 caracteres",
+		}
+
+		return echoContext.Render(http.StatusInternalServerError, "getCredentials", viewModel)
+	}
+
+	credentials.SetPasswordHash()
 	repository.SaveCredentials(credentials)
-	return echoContext.Render(http.StatusOK, "getCredentials", "")
+	return echoContext.Render(http.StatusOK, "getCredentials", nil)
+}
+
+type GetCredentialViewModel struct {
+	HasError     bool
+	ErrorMessage string
 }
--- models/credentials.go
+++ models/credentials.go
@@ -1,6 +1,37 @@
 package models
 
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type Credentials struct {
-	Email    string `form:"email"`
-	Password string `form:"password"`
+	Email              string `form:"email"`
+	Password           string `form:"password"`
+	PasswordMD5Hash    string
+	PasswordSha1Hash   string
+	PasswordSha256Hash string
+	PasswordSha512Hash string
+	PasswordBcryptHash string
+}
+
+func (c *Credentials) SetPasswordHash() {
+	plainPassword := []byte(c.Password)
+
+	md5Hash := md5.Sum(plainPassword)
+	sha1Hash := sha1.Sum(plainPassword)
+	sha256Hash := sha256.Sum256(plainPassword)
+	sha512Hash := sha512.Sum512(plainPassword)
+	bcryptHash, _ := bcrypt.GenerateFromPassword(plainPassword, bcrypt.DefaultCost)
+
+	c.PasswordMD5Hash = hex.EncodeToString(md5Hash[:])
+	c.PasswordSha1Hash = hex.EncodeToString(sha1Hash[:])
+	c.PasswordSha256Hash = hex.EncodeToString(sha256Hash[:])
+	c.PasswordSha512Hash = hex.EncodeToString(sha512Hash[:])
+	c.PasswordBcryptHash = string(bcryptHash)
 }
