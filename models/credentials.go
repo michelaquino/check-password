0f3cab6f8b64c5696b2b7b336d7cb46a855f54d5
Michel Aquino <michel.aquino@corp.globo.com>
2017-08-15 19:01:31 -0300

    Check if email was leak

--- models/credentials.go
+++ models/credentials.go
@@ -6,12 +6,18 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"gitlab.globoi.com/michel.aquino/check-password/context"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 type Credentials struct {
 	Email              string `form:"email" bson:"email,omitempty"`
+	EmailLeaked        bool   `bson:"emailLeaked,omitempty"`
 	Password           string `form:"password"`
 	PasswordMD5Hash    string `bson:"passwordMD5Hash,omitempty"`
 	PasswordSha1Hash   string `bson:"passwordSha1Hash,omitempty"`
@@ -34,4 +40,36 @@ func (c *Credentials) SetPasswordHash() {
 	c.PasswordSha256Hash = hex.EncodeToString(sha256Hash[:])
 	c.PasswordSha512Hash = hex.EncodeToString(sha512Hash[:])
 	c.PasswordBcryptHash = string(bcryptHash)
+}
+
+func (c *Credentials) CheckEmailLeak() {
+	log := context.GetLogger()
+
+	url := fmt.Sprintf("https://haveibeenpwned.com/api/v2/breachedaccount/%s", c.Email)
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Error("Build request object", "Error", err.Error())
+		return
+	}
+
+	httpClient := http.Client{}
+	response, err := httpClient.Do(request)
+	if err != nil {
+		log.Error("Make request to haveibeenpwned", "Error", err.Error())
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		log.Info("Verify response code", "Error", fmt.Sprintf("Response code: %d", response.StatusCode))
+		return
+	}
+
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Error("Read response body", "Error", err.Error())
+		return
+	}
+
+	fmt.Println("Response body: ", string(responseBody))
 }
